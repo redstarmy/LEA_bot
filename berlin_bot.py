60e406b5c8299c33cb67e505c3fb06d12afbe2bf
redstarmy <58826939+redstarmy@users.noreply.github.com>
2023-08-01 12:07:42 +0200

    streamlined input and prevent timeout on long response times

--- berlin_bot.py
+++ berlin_bot.py
@@ -1,154 +1,152 @@
-import time
-import os
 import logging
-from platform import system
+import time
 
+from playsound import playsound
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
-
-
-system = system()
+from selenium.webdriver.support.wait import WebDriverWait
 
 logging.basicConfig(
     format='%(asctime)s\t%(levelname)s\t%(message)s',
     level=logging.INFO,
 )
 
+
 class WebDriver:
     def __init__(self):
         self._driver: webdriver.Chrome
-        self._implicit_wait_time = 20
+        self._implicit_wait_time = 400
 
     def __enter__(self) -> webdriver.Chrome:
-        logging.info("Open browser")
-        # some stuff that prevents us from being locked out
-        options = webdriver.ChromeOptions() 
+        logging.info("Opening browser")
+        # prevent selenium detection
+        options = webdriver.ChromeOptions()
         options.add_argument('--disable-blink-features=AutomationControlled')
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_experimental_option('useAutomationExtension', False)
+        # let selenium manager get chromedriver
         self._driver = webdriver.Chrome(options=options)
-        self._driver.implicitly_wait(self._implicit_wait_time) # seconds
+        self._driver.implicitly_wait(self._implicit_wait_time)  # seconds
         self._driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
-        self._driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
+        self._driver.execute_cdp_cmd('Network.setUserAgentOverride', {
+            "userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
         return self._driver
 
     def __exit__(self, exc_type, exc_value, exc_tb):
-        logging.info("Close browser")
+        logging.info("Closing browser")
         self._driver.quit()
 
+
 class BerlinBot:
-    def __init__(self):
-        self.wait_time = 20
-        self._sound_file = os.path.join(os.getcwd(), "alarm.wav")
+    def __init__(self, citizenship, applicants, family, appointment_type, reason_category, reason_type,
+                 citizenship_family=None):
+        # individual parameters
+        self.citizenship = citizenship
+        self.applicants = applicants
+        self.family = family
+        self.citizenship_family = citizenship_family
+        self.appointment_type = appointment_type
+        self.reason_category = reason_category
+        self.reason_type = reason_type
+        # max timeout
+        self.timeout = 400
+        # time before resubmitting form
+        self.cycle_time = 20
         self._error_message = """Für die gewählte Dienstleistung sind aktuell keine Termine frei! Bitte"""
 
-    @staticmethod
-    def enter_start_page(driver: webdriver.Chrome):
-        logging.info("Visit start page")
-        driver.get("https://otv.verwalt-berlin.de/ams/TerminBuchen")
-        driver.find_element(By.XPATH, '//*[@id="mainForm"]/div/div/div/div/div/div/div/div/div/div[1]/div[1]/div[2]/a').click()
-        time.sleep(5)
-
-    @staticmethod
-    def tick_off_some_bullshit(driver: webdriver.Chrome):
-        logging.info("Ticking off agreement")
-        driver.find_element(By.XPATH, '//*[@id="xi-div-1"]/div[4]/label[2]/p').click()
-        time.sleep(1)
+    def proceed(self, driver):
+        # wait for proceed button to be clickable
+        WebDriverWait(driver, self.timeout).until(
+            EC.invisibility_of_element_located((By.CLASS_NAME, "loading")))
+        # press button to proceed
         driver.find_element(By.ID, 'applicationForm:managedForm:proceed').click()
-        time.sleep(5)
+        # wait for loading to finish before continuing
+        WebDriverWait(driver, self.timeout).until(
+            EC.invisibility_of_element_located((By.CLASS_NAME, "loading")))
 
     @staticmethod
-    def enter_form(driver: webdriver.Chrome):
-        logging.info("Fill out form")
-        # select china
-        s = Select(driver.find_element(By.ID, 'xi-sel-400'))
-        s.select_by_visible_text("China")
-        # eine person
-        s = Select(driver.find_element(By.ID, 'xi-sel-422'))
-        s.select_by_visible_text("eine Person")
-        # no family
-        s = Select(driver.find_element(By.ID, 'xi-sel-427' ))
-        s.select_by_visible_text("nein")
-        time.sleep(5)
+    def visit_start_page(driver: webdriver.Chrome):
+        logging.info("Visiting start page")
+        driver.get("https://otv.verwalt-berlin.de/ams/TerminBuchen")
+        driver.find_element(By.XPATH, "//a[text()='Termin buchen']").click()
 
-        # extend stay
-        driver.find_element(By.XPATH, '//*[@id="xi-div-30"]/div[2]/label/p').click()
+    def tick_off_agreement(self, driver: webdriver.Chrome):
+        logging.info("Ticking off agreement")
+        driver.find_element(By.ID, 'xi-cb-1').click()
+        self.proceed(driver)
+
+    def fill_form(self, driver: webdriver.Chrome):
+        logging.info("Filling out form")
+        # select citizenship
+        Select(driver.find_element(By.ID, 'xi-sel-400')).select_by_visible_text(self.citizenship)
+        # select number of applicants
+        Select(driver.find_element(By.ID, 'xi-sel-422')).select_by_visible_text(self.applicants)
+        # select family member
+        Select(driver.find_element(By.ID, 'xi-sel-427')).select_by_visible_text(self.family)
+
+        if self.family == "ja":
+            Select(driver.find_element(By.ID, 'xi-sel-428')).select_by_visible_text(self.citizenship_family)
+
         time.sleep(2)
 
-        # click on study group
-        driver.find_element(By.XPATH, '//*[@id="inner-479-0-2"]/div/div[1]/label/p').click()
+        # select appointment type
+        driver.find_element(By.XPATH, f'//*[@id="xi-div-30"]/div[{self.appointment_type}]').click()
         time.sleep(2)
 
-        # b/c of stufy
-        driver.find_element(By.XPATH, '//*[@id="inner-479-0-2"]/div/div[2]/div/div[5]/label').click()
-        time.sleep(4)
+        # select reason category
+        driver.find_element(By.XPATH, f'//*[@id="inner-436-0-1"]/div/div[{self.reason_category}]').click()
+        time.sleep(2)
+
+        # select reason type
+        driver.find_element(By.XPATH, f'//*[@id="inner-436-0-1"]/div/div[2]/div/div[{self.reason_type}]/label').click()
+        time.sleep(2)
 
         # submit form
-        driver.find_element(By.ID, 'applicationForm:managedForm:proceed').click()
-        time.sleep(10)
-    
-    def _success(self):
-        logging.info("!!!SUCCESS - do not close the window!!!!")
-        while True:
-            self._play_sound_osx(self._sound_file)
-            time.sleep(15)
-        
-        # todo play something and block the browser
+        self.proceed(driver)
 
+    def _is_success(self, driver):
+        terminauswahl = driver.find_element(By.XPATH,
+                                            '//*[@id="main"]/div[2]/div[4]/div[2]/div/div[1]/ul/li[3]')
 
-    def run_once(self):
+        if self._error_message not in driver.page_source and 'antcl_active' in terminauswahl.get_attribute(
+                'class').split():
+            logging.info("!!!SUCCESS - do not close the window!!!!")
+            while True:
+                playsound('alarm.wav')
+                time.sleep(15)
+
+    def cycle(self):
         with WebDriver() as driver:
-            self.enter_start_page(driver)
-            self.tick_off_some_bullshit(driver)
-            self.enter_form(driver)
+            self.visit_start_page(driver)
+            self.tick_off_agreement(driver)
+            self.fill_form(driver)
 
-            # retry submit
+            # retry submitting form
             for _ in range(10):
-                if not self._error_message in driver.page_source:
-                    self._success()
-                logging.info("Retry submitting form")
-                driver.find_element(By.ID, 'applicationForm:managedForm:proceed').click()
-                time.sleep(self.wait_time)
+                # are appointments available?
+                self._is_success(driver=driver)
+                time.sleep(self.cycle_time)
 
-    def run_loop(self):
-        # play sound to check if it works
-        self._play_sound_osx(self._sound_file)
+                logging.info("Resubmitting form")
+                self.proceed(driver)
+
+    def start(self):
         while True:
-            logging.info("One more round")
-            self.run_once()
-            time.sleep(self.wait_time)
+            logging.info("Starting new cycle")
+            self.cycle()
+            time.sleep(self.cycle_time)
 
-    # stolen from https://github.com/JaDogg/pydoro/blob/develop/pydoro/pydoro_core/sound.py
-    @staticmethod
-    def _play_sound_osx(sound, block=True):
-        """
-        Utilizes AppKit.NSSound. Tested and known to work with MP3 and WAVE on
-        OS X 10.11 with Python 2.7. Probably works with anything QuickTime supports.
-        Probably works on OS X 10.5 and newer. Probably works with all versions of
-        Python.
-        Inspired by (but not copied from) Aaron's Stack Overflow answer here:
-        http://stackoverflow.com/a/34568298/901641
-        I never would have tried using AppKit.NSSound without seeing his code.
-        """
-        from AppKit import NSSound
-        from Foundation import NSURL
-        from time import sleep
-
-        logging.info("Play sound")
-        if "://" not in sound:
-            if not sound.startswith("/"):
-                from os import getcwd
-
-                sound = getcwd() + "/" + sound
-            sound = "file://" + sound
-        url = NSURL.URLWithString_(sound)
-        nssound = NSSound.alloc().initWithContentsOfURL_byReference_(url, True)
-        if not nssound:
-            raise IOError("Unable to load sound named: " + sound)
-        nssound.play()
-
-        if block:
-            sleep(nssound.duration())
 
 if __name__ == "__main__":
-    BerlinBot().run_loop()
+    citizenship = "Indien"
+    applicants = "eine Person"
+    family = "nein"
+    appointment_type = 1
+    reason_category = 1
+    reason_type = 5
+
+    BerlinBot(citizenship=citizenship, applicants=applicants, family=family, appointment_type=appointment_type,
+              reason_category=reason_category,
+              reason_type=reason_type).start()
